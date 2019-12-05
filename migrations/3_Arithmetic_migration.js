82a1a6892876bd6eac39d57db90f00a4ee4df868
lakshmikanth <kanth.explorer@gmail.com>
2019-12-05 14:27:19 +0800

    fixes migration script for Arithmetic Proxy

--- migrations/3_Arithmetic_migration.js
+++ migrations/3_Arithmetic_migration.js
@@ -3,22 +3,7 @@ const arithmetic = artifacts.require('../contracts/arithmetic/Arithmetic');
 const arithmeticStorage = artifacts.require('../contracts/arithmetic/ArithmeticStorage');
 
 module.exports = async function(deployer, networks, accounts) {
-  const arithmeticStorage = await deployer.deploy(arithmeticStorage).then(async a => {
-    console.log('arithmeticStorage contract is deployed : '+a.address);
-    let con = new web3.eth.Contract(a.abi, a.address, { address: a.address });
-    const arithmeticStorageAddress = a.address;
-    console.log('deployed arithmeticStorage with address: '+arithmeticStorageAddress);
-    });
-
-  const arithmetic = await deployer.deploy(arithmetic).then(async a => {
-    console.log('arithmeticProxy contract is deployed : '+a.address);
-    let con = new web3.eth.Contract(a.abi, a.address, { address: a.address });
-    const arithmeticAddress = a.address;
-    console.log('deployed arithmeticProxy with address: '+arithmeticAddress);
-    const constructorData = con.methods.initialize('ArithmeticLogic').encodeABI();
-    console.log('deployed arithmeticProxy with constructor data: '+constructorData);
-    });
-
+ 
   deployer.deploy(arithmeticProxy).then(async a => {
     console.log('arithmeticProxy contract is deployed : '+a.address);
     let con = new web3.eth.Contract(a.abi, a.address, { address: a.address });
